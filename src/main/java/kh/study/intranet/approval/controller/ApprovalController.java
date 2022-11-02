fbfbd0d2f18b9ad35f27b68b97e1fc34ce5faa5a
juo0o <dlqmsdnfgnsi@nate.com>
2022-11-02 16:45:58 +0900

    approvalCommit

--- src/main/java/kh/study/intranet/approval/controller/ApprovalController.java
+++ src/main/java/kh/study/intranet/approval/controller/ApprovalController.java
@@ -1,13 +1,24 @@
 package kh.study.intranet.approval.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/approval")
 public class ApprovalController {
-
-	public void aaa() {
+	
+	//결재리스트게시판
+	@GetMapping("/approvalBoard")
+	public String approvalBoard() {
 		
+		return "pages/approval/approval_board";
+	}
+	
+	//결재양식작성페이지
+	@GetMapping("/writeApproval")
+	public String writeApproval() {
+		
+		return "pages/approval/write_approval";
 	}
 }
